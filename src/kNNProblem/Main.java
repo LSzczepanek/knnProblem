5fe85f3b388d8b2f6147b580dc48af5d4ab69a47
Lukasz Szczepanek <lukas.szczepanek@gmail.com>
2016-10-02 23:57:54 +0200

    Declaration of kNN Algorythm

--- src/kNNProblem/Main.java
+++ src/kNNProblem/Main.java
@@ -8,10 +8,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String testFile = null;
-		ObjectToUse[] testFiles = new ObjectToUse[25];
 		System.out.println("Program will show how good or how bad the kNN Algorythm it is for such a data like IRYS");
 		try {
-			testFile = FileHelper.readFile("G:\\ProjektyEclipse\\kNNProblem\\src\\resources\\test.txt");
+			testFile = FileHelper.readFile("C:/eclipse-java-neon-1-win32-x86_64/workspace/knnProblem/src/resources/test.txt");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -20,12 +19,15 @@ public class Main {
 		testFile = testFile.replaceAll("\\n", "");
 		System.out.println(testFile);
 		String[] separatedString = testFile.split(",");
+		
+		ObjectToUse[] testFiles = new ObjectToUse[separatedString.length/3];
+		
 		//testFiles[0] = new ObjectToUse();
 		
 //		System.out.println(separatedString2[0]);
 //		System.out.println(separatedString2[1]);
 //		System.out.println(separatedString2[2]);
-		for (int i=0 , j=0; j <25; i+=3, j++) {
+		for (int i=0 , j=0; j <testFiles.length; i+=3, j++) {
 			testFiles[j] = new ObjectToUse(Double.parseDouble(separatedString[i]),
 					Double.parseDouble(separatedString[i+1]), 
 					0.0, 
